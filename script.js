5aba5bbb8791131d2e274b7429199826d352b6ce
Freeplay <kashifboss8585@gmail.com>
2026-02-06 17:50:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let clickCount = 0;
 
+// Ads links (2 clicks required)
 const adLinks = [
   "https://www.effectivegatecpm.com/w8grsbmi3?key=bad7a3a33307c1c056108ce6671cb556",
   "https://www.effectivegatecpm.com/w8grsbmi3?key=bad7a3a33307c1c056108ce6671cb556"
@@ -11,11 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const claimBtn = document.getElementById("claimBtn");
 
   claimBtn.addEventListener("click", () => {
-    if (clickCount < adLinks.length) {
+    if(clickCount < adLinks.length){
+      // Open ad in new tab
       window.open(adLinks[clickCount], "_blank");
       clickCount++;
 
-      if (clickCount === adLinks.length) {
+      // After 2nd click, show success
+      if(clickCount === adLinks.length){
         setTimeout(() => {
           mainBox.classList.add("hidden");
           successBox.classList.remove("hidden");
